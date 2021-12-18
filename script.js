6397d344122044e41738c052176c8c87826dc1e7
Timespike <50344633+Timespike@users.noreply.github.com>
2021-12-17 20:02:30 -0600

    starting javascript portion

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        something
+    } catch (error) {
+        // Catch Error Here
+        console.log('whoops, error here:', error);
+    }
+}
+
+// On Load
+selectMediaStream();
